d03f535a3115c9c03de65de121d4b88d4f205bf5
DH9905 <dlgnsdl99@gmail.com>
2024-12-20 18:16:13 +0900

    삭제 후 다시

--- scit_task/src/main/java/com/app/scit_task/Repository/TestRepository.java
+++
@@ -1,7 +0,0 @@
-package com.app.scit_task.Repository;
-
-import com.app.scit_task.Entity.TestEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TestRepository extends JpaRepository<TestEntity, Integer> {
-}
--- scit_task/src/main/java/com/app/scit_task/Repository/TodoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.app.scit_task.Repository;
-
-import com.app.scit_task.Entity.TodoEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TodoRepository extends JpaRepository<TodoEntity, Long> {
-}
